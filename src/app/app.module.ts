1e9bd8b484fcf8bb05288ebdce3ff6686593e718
jepkess <biivincent1997@gmail.com>
2021-10-18 10:33:35 +0300

    generate notfpundcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { FormComponent } from './form/form.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    FormComponent
+    FormComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
